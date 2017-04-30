29d03daeaca68fce80421867b4542ad11c5ae290
WellingtonCosta <wellington.costa128@gmail.com>
2017-04-30 16:53:56 -0300

    Models.

--- app/src/main/java/br/com/wellingtoncosta/mymovies/domain/FavoriteMovie.java
+++ app/src/main/java/br/com/wellingtoncosta/mymovies/domain/FavoriteMovie.java
@@ -1,31 +1,37 @@
 package br.com.wellingtoncosta.mymovies.domain;
 
-import lombok.Data;
-
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-
 /**
  * @author Wellington Costa on 30/04/17.
  */
-@Data
-@Entity
-@Table(name = "favorite_movie")
 public class FavoriteMovie {
 
-    @Id
-    @NotNull
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "favorite_movie_sequence")
-    @SequenceGenerator(name = "favorite_movie_sequence", sequenceName = "favorite_movie_id_seq", allocationSize = 1)
     private Long id;
 
-    @NotNull
-    @JoinColumn(name = "movie")
-    @ManyToOne(fetch = FetchType.LAZY)
     private Movie movie;
 
-    @NotNull
-    @JoinColumn(name = "user")
-    @ManyToOne(fetch = FetchType.LAZY)
     private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- app/src/main/java/br/com/wellingtoncosta/mymovies/domain/Movie.java
+++ app/src/main/java/br/com/wellingtoncosta/mymovies/domain/Movie.java
@@ -1,33 +1,16 @@
 package br.com.wellingtoncosta.mymovies.domain;
 
-import lombok.Data;
-
-import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-
 /**
- * @author Wellington Costa on 29/04/17.
+ * @author Wellington Costa on 30/04/17.
  */
-@Data
-@Entity
-@Table(name = "movie")
 public class Movie {
 
-    @Id
-    @NotNull
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "movie_sequence")
-    @SequenceGenerator(name = "movie_sequence", sequenceName = "movie_id_seq", allocationSize = 1)
     private Long id;
 
-    @Column
-    @NotNull
     private String name;
 
-    @Column
-    @NotNull
     private String year;
 
-    @Column
     private String description;
 
 }
