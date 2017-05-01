a7e66dcdb0f72906c9f0f15b5bcfaa151ad53458
WellingtonCosta <wellington.costa128@gmail.com>
2017-05-01 13:35:39 -0300

    Build flavor / OkHttp Client

--- app/src/main/java/br/com/wellingtoncosta/mymovies/dagger/NetworkModule.java
+++ app/src/main/java/br/com/wellingtoncosta/mymovies/dagger/NetworkModule.java
@@ -4,11 +4,15 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.inject.Singleton;
 
+import br.com.wellingtoncosta.mymovies.BuildConfig;
 import br.com.wellingtoncosta.mymovies.retrofit.UserService;
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -28,10 +32,20 @@ public class NetworkModule {
 
     @Provides
     @Singleton
-    Retrofit provideRetrofit(Gson gson) {
+    OkHttpClient provideOkHttpClient() {
+     return new OkHttpClient.Builder()
+                .readTimeout(60, TimeUnit.SECONDS)
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .build();
+    }
+
+    @Provides
+    @Singleton
+    Retrofit provideRetrofit(Gson gson, OkHttpClient okHttpClient) {
         return new Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create(gson))
-                .baseUrl("https://my-movies-api.herokuapp.com/")
+                .baseUrl(BuildConfig.API_URL)
+                .client(okHttpClient)
                 .build();
     }
 
