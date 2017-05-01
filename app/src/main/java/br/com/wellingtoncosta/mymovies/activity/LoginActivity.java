099f00972705bbaac7a87a9978cd2a95a3119ae6
WellingtonCosta <wellington.costa128@gmail.com>
2017-05-01 15:03:28 -0300

    Login validation

--- app/src/main/java/br/com/wellingtoncosta/mymovies/activity/LoginActivity.java
+++ app/src/main/java/br/com/wellingtoncosta/mymovies/activity/LoginActivity.java
@@ -2,15 +2,22 @@ package br.com.wellingtoncosta.mymovies.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Patterns;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import javax.inject.Inject;
 
 import br.com.wellingtoncosta.mymovies.Application;
 import br.com.wellingtoncosta.mymovies.R;
+import br.com.wellingtoncosta.mymovies.domain.User;
 import br.com.wellingtoncosta.mymovies.retrofit.UserService;
+import br.com.wellingtoncosta.mymovies.validation.Validation;
+import br.com.wellingtoncosta.mymovies.validation.validators.EmailValidator;
+import br.com.wellingtoncosta.mymovies.validation.validators.NotEmptyValidator;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -24,16 +31,46 @@ public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.toolbar)
     Toolbar toolbar;
 
+    @BindView(R.id.emailLayout)
+    TextInputLayout emailLayout;
+
+    @BindView(R.id.emailField)
+    EditText emailField;
+
+    @BindView(R.id.passwordLayout)
+    TextInputLayout passwordLayout;
+
+    @BindView(R.id.passwordField)
+    EditText passwordField;
+
     @Inject
     UserService userService;
 
+    private Validation validation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ((Application) getApplication()).component().inject(LoginActivity.this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         setSupportActionBar(toolbar);
         ButterKnife.bind(this);
-        ((Application) getApplication()).component().inject(LoginActivity.this);
+        setupValidation();
+    }
+
+    private void setupValidation() {
+        validation = new Validation();
+
+        validation.addValidator(new EmailValidator(
+                emailLayout,
+                Patterns.EMAIL_ADDRESS.pattern(),
+                getString(R.string.invalid_email)
+        ));
+
+        validation.addValidator(new NotEmptyValidator(
+                passwordLayout,
+                getString(R.string.field_required)
+        ));
     }
 
     @OnEditorAction(R.id.passwordField)
@@ -44,7 +81,18 @@ public class LoginActivity extends AppCompatActivity {
 
     @OnClick(R.id.loginButton)
     public void login() {
-        Toast.makeText(this, "Logar", Toast.LENGTH_LONG).show();
+        if (validation.validate()) {
+            User user = buildUser(); // TODO call API login
+            Toast.makeText(this, "Logar", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private User buildUser() {
+        User user = new User();
+        user.setEmail(emailField.getText().toString());
+        user.setPassword(passwordField.getText().toString()); // TODO encrypt password
+
+        return user;
     }
 
     @OnClick(R.id.registerNewUserButton)
