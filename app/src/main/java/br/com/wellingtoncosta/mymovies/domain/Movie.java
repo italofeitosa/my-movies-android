45836e557873c2a04c74c2bf814313a4e7603189
WellingtonCosta <wellington.costa128@gmail.com>
2017-05-06 17:39:32 -0300

    Movie genre

--- app/src/main/java/br/com/wellingtoncosta/mymovies/domain/Movie.java
+++ app/src/main/java/br/com/wellingtoncosta/mymovies/domain/Movie.java
@@ -15,6 +15,39 @@ public class Movie extends RealmObject {
 
     private String year;
 
-    private String description;
+    private String genre;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 
 }
